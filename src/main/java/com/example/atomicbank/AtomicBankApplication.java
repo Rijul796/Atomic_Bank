0ebaa29bbae5f8baa8c569cb386732b00c98650c
rijul <rijulsharma.1972@gmail.com>
2026-01-28 21:40:41 +0530

    Initial Commit

---
+++ src/main/java/com/example/atomicbank/AtomicBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.atomicbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atomicbank/AtomicBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atomicbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomicBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
